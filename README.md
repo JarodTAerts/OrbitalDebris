53f12a73f0ca12a3ba3b3c555ca0720ab10ec02d
JarodAert <jarod.aerts@huskers.unl.edu>
2019-02-24 15:24:16 -0600

    Updated readme and made some constants to get things to work better

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# OrbitalDebris
-Repo for all research of orbital debris and their effect on the earth's atmosphere and climate.
+# Orbital Debris
+This is a repo which contains my work with orbital space debris and its effect on earth's atmosphere and enviroment. All source code for this project is in python. This code is completely open sourced and can be used by anyone for any purpose.
+
+## Required Python Packages
+
+- Numpy
+  - Used for various calculations.
+  - Installation: ```pip install numpy```
+- MayAvi MLab
+  - Used for ploting and displaying the orbits in 3d space.
+  - Installation: ```pip install mayavi```
+- Pandas
+  - Used for reading in the data from a csv file and organizing it with pandas dataframes.
+  -  Installation: ```pip install pandas```
+
+## Running the Simulation
+
+1. Install all required python packages noted above.
+2. Navigate to the __./OrbitalDebris__ directory.
+3. Run the __orbitPlotter.py__ python script with python. Something like: ```python ./source/orbitPlotter.py```.
+
+## Notes
+- If you decide to run the script from a different location, such as inside the source folder, you may have to change the constants __DATA_FILE_PATH__ and __EARTH_IMAGE_PATH__ in __orbitPlotter.py__ so the can be properly loaded.
+- If you choose to plot a different number of orbits by changing __NUM_ORBITS_PLOTTED__ in __orbitPlotter.py__ it may take a very long time to load due to the slow render times with MayAvi.
+- The thick black orbit shown in the plot is the average orbit of all orbits in the dataset.
+
+## Sources
+- Datafile containing all the orbits of the orbital debris can be found here <https://www.space-track.org/auth/login>. A valid account will be needed to access data.
+- Earth Image is NASA's Blue Marble texture image and can be found here <https://visibleearth.nasa.gov/view.php?id=73909>.
--- source/orbitPlotter.py
+++ source/orbitPlotter.py
@@ -1,15 +1,17 @@
 import numpy as np
 from mayavi import mlab
-import numpy as np
 import pandas as pd
 import math
 from tvtk.api import tvtk
 from Debris import Debris
 
 # Set constants for size of earth
-earthDiameter=12742
-earthR=earthDiameter/2
-earthTilt=23.5
+EARTH_D=12742
+EARTH_R=EARTH_D/2
+EARTH_TILT=23.5
+DATA_FILE_PATH="./data/debris.csv"
+EARTH_IMAGE_PATH="./images/earth.jpg"
+NUM_ORBITS_PLOTTED=1000
 
 def GetAverageOrbit(listODebris):
     features=[0,0,0,0]
@@ -23,7 +25,7 @@ def GetAverageOrbit(listODebris):
 
 
 # Load in data from CSV file, and get rid of all items no longer in orbit or with bad data
-debrisDF=pd.read_csv('./debris.csv')
+debrisDF=pd.read_csv(DATA_FILE_PATH)
 debrisDF=debrisDF.loc[(debrisDF['DECAY'].isnull()) & (np.isfinite(debrisDF['INCLINATION'])) & (np.isfinite(debrisDF['APOGEE'])) & (np.isfinite(debrisDF['PERIGEE']))]
 # Fix size column where there is no data
 debrisDF.fillna({'RCS_SIZE': ""})
@@ -36,10 +38,10 @@ for index, row in debrisDF.iterrows():
 
 # Get the texture for earth
 textureReader = tvtk.JPEGReader()
-textureReader.file_name='earth2.jpg'
+textureReader.file_name=EARTH_IMAGE_PATH
 texture = tvtk.Texture(input_connection=textureReader.output_port, interpolate=1)
 # create the sphere source with a given radius and angular resolution
-sphere = tvtk.TexturedSphereSource(radius=earthR, theta_resolution=180,phi_resolution=180)
+sphere = tvtk.TexturedSphereSource(radius=EARTH_R, theta_resolution=180,phi_resolution=180)
 # assemble rest of the pipeline, assign texture    
 sphere_mapper = tvtk.PolyDataMapper(input_connection=sphere.output_port)
 sphere_actor = tvtk.Actor(mapper=sphere_mapper, texture=texture)
@@ -47,15 +49,15 @@ mlab.gcf().scene.add_actor(sphere_actor)
 sphere_actor.rotate_y(23.5)
 
 # Plot the orbits of the objects in the debris list
-for i in range(0, 10):
-    debrisList[i].PlotOrbit(mlab, earthDiameter, 10, earthTilt)
+for i in range(0, NUM_ORBITS_PLOTTED):
+    debrisList[i].PlotOrbit(mlab, EARTH_D, 10, EARTH_TILT)
 
 averageDeb=GetAverageOrbit(debrisList)
-averageDeb.PlotOrbit(mlab, earthDiameter, 100, earthTilt)
+averageDeb.PlotOrbit(mlab, EARTH_D, 100, EARTH_TILT)
 
 deb=Debris("Test Debris", "DEBRIS", 90, 10, 1000, 1000, "")
-print(deb.GetYearlyTimeSpentAtLatitudes(earthR, earthTilt))
-deb.PlotOrbit(mlab, earthDiameter, 200, earthTilt)
+print(deb.GetYearlyTimeSpentAtLatitudes(EARTH_R, EARTH_TILT))
+deb.PlotOrbit(mlab, EARTH_D, 200, EARTH_TILT)
 
 # Set camera to focus on origin point and show the plot
 mlab.view(focalpoint=(0,0,0))
